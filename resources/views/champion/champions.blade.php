3d7b8dd2ff46d6735115e3893c145db1ae588c16
Daniel Primitivo Cano <dpc32@alu.ua.es>
2019-04-05 11:46:13 +0200

    modificado diseño checkbox campeones

--- resources/views/champion/champions.blade.php
+++ resources/views/champion/champions.blade.php
@@ -10,19 +10,21 @@
 			</div>
 		</div>
 	</div>
-	<div class="row justify-content-center">
-		<div class="col align-self-center">
-			<div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-				<div class="form-check">
-					<form method="GET" action="{{ route('champions') }}">
+	<div class="row justify-content-center align-items-center space">
+        <div class="col-sm-6 col-lg-3">	
+			<form method="GET" action="{{ route('champions') }}">
+				<div class="form-group row align-items-center ">
+					<div class="col-sm-2 col-lg-3">
 						<input class="form-check-checkbox" type="checkbox" id="order" name="order" {{ $check }}>
+					</div>
+					<div class="col-sm-10 col-lg-3">
 						<input class="btn btn-primary" type="submit" value="Ordenar alfabeticamente">
-					</form>
+					</div>
 				</div>
-			</div>
+			</form>
 		</div>
 	</div>
-	<div class="row justify-content-center space">
+	<div class="row justify-content-center littlespace">
 		@foreach ($champions as $champion)
 			<div class="col-lg-2 col-md-3 col-sm-4 col-5">
 				<div class="features-icons-item mb-5 mb-lg-0 mb-lg-3">
