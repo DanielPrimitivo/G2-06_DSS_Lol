987ed52208f775fc2e691257c9e36f75f645c37e
Enrique Miguel Juan Fuster <emjf1@alu.ua.es>
2019-05-19 17:36:30 +0200

    Pasar información rune page

--- app/RunePage.php
+++ app/RunePage.php
@@ -111,7 +111,35 @@ class RunePage extends Model
     }
 
     public static function informacionIndividual(RunePage $runePage){
-        return view('runePage.runePage', compact('runePage'));
+        $runesRunesPages = DB::table('rune_runePages')->where('runePage_id', '=', $runePage->id)->get();
+        $t1 = ""; $t2 = "";
+        $rune1;$rune2;$rune3;$rune4;$rune5;$rune6;
+        $i = 1;
+        foreach($runesRunesPages as $runeRunePage){
+            $rune = Rune::find($runeRunePage->rune_id);
+            if($i == 1){
+                $rune1 = $rune;
+            }else if($i == 2){
+                $rune2 = $rune;
+            }else if($i == 3){
+                $rune3 = $rune;
+            }else if($i == 4){
+                $rune4 = $rune;
+            }else if($i == 5){
+                $rune5 = $rune;
+            }else if($i == 6){
+                $rune6 = $rune;
+            }
+            if($t1 == ""){
+                $t1 = $rune->type;
+            }else if($rune->type != $t1){
+                    $t2 = $rune->type;
+            }
+            $i += 1;
+        }
+        $runesPrimary = array($rune1, $rune2, $rune3, $rune4);
+        $runesSecundary = array($rune5, $rune6);
+        return view('runePage.runePage', compact('runePage', 't1', 't2', 'runesPrimary', 'runesSecundary'));
     }
 
     public static function ordenarAlfabeticamente(){
